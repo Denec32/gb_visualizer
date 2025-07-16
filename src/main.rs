01485bd1acb3bfd44d6c737b7f2da60d21548c47
denec <dzenevich@mail.ru>
2025-07-16 20:47:11 +0300

    basic control flow when parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+use std::collections::{BTreeMap, VecDeque};
 use std::fs;
-use std::fs::File;
-use std::io::Write;
 
 trait InstructionMatcher {
     fn is_match(&self, opcode: u8) -> bool;
@@ -28,213 +27,301 @@ impl InstructionMatcher for str {
 }
 struct CartridgeReader {
     data: Vec<u8>,
-    pos: usize,
 }
 
 impl CartridgeReader {
     fn new(data: Vec<u8>) -> CartridgeReader {
-        CartridgeReader { data, pos: 0x100 }
+        CartridgeReader { data }
     }
 
-    fn has_next(&self) -> bool {
-        self.pos < self.data.len()
+    fn read_instruction(&mut self, pos: usize) -> u8 {
+        self.data[pos]
     }
 
-    fn get_next(&mut self) -> u8 {
-        let opcode = self.data[self.pos];
-        self.pos += 1;
-        opcode
+    fn read_imm8(&mut self, pos: usize) -> u8 {
+        self.data[pos + 1]
     }
 
-    fn get_imm16(&mut self) -> u16 {
-        let higher_nimble = self.get_next();
-        let lower_nimble = self.get_next();
+    fn read_imm16(&mut self, pos: usize) -> u16 {
+        let higher_nimble = self.data[pos + 1];
+        let lower_nimble = self.data[pos + 2];
 
         (lower_nimble as u16) << 8 | higher_nimble as u16
-    }
-
-    fn set_position(&mut self, pos: usize) {
-        self.pos = pos;
-    }
-
-    fn get_imm8(&mut self) -> u8 {
-        self.get_next()
     }
 }
 
 fn main() {
     let cartridge_name = "hello-world.gb";
     let content = fs::read(cartridge_name).unwrap();
-
-    let mut file = File::create("hello-world.txt").unwrap();
-
     let mut cartridge = CartridgeReader::new(content);
 
-    while cartridge.has_next() {
-        let opcode = cartridge.get_next();
+    let mut visited = BTreeMap::new();
 
-        let line = if "00000000".is_match(opcode) {
-            "nop".to_string()
+    let mut queue = VecDeque::new();
+    queue.push_front(0x100usize);
+
+    while !queue.is_empty() {
+        let pos = queue.pop_back().unwrap();
+        if visited.contains_key(&pos) {
+            continue;
+        }
+
+        let opcode = cartridge.read_instruction(pos);
+
+        if "00000000".is_match(opcode) {
+            // nop
+            queue.push_front(pos);
+            visited.insert(pos, "nop".to_string());
         } else if "00..0001".is_match(opcode) {
-            cartridge.get_next();
-            cartridge.get_next();
-            "ld r16, imm16".to_string()
+            //ld r16, imm16
+            let imm16 = cartridge.read_imm16(pos);
+            queue.push_front(pos + 3);
+            visited.insert(pos, format!("ld r16, [{:x}]", imm16));
         } else if "00..0010".is_match(opcode) {
-            "ld [r16mem], a".to_string()
+            //ld [r16mem], a
+            queue.push_front(pos + 1);
+            visited.insert(pos, "ld [r16mem], a".to_string());
         } else if "00..1010".is_match(opcode) {
-            "ld a, [r16mem]".to_string()
+            //ld a, [r16mem]
+            queue.push_front(pos + 1);
+            visited.insert(pos, "ld a, [r16mem]".to_string());
         } else if "00001000".is_match(opcode) {
-            cartridge.get_next();
-            cartridge.get_next();
-            "ld [imm16], sp".to_string()
+            //ld [imm16], sp
+            let imm16 = cartridge.read_imm16(pos);
+            queue.push_front(pos + 3);
+            visited.insert(pos, format!("ld [{:x}], sp", imm16));
         } else if "00..0011".is_match(opcode) {
-            "inc r16".to_string()
+            //inc r16
+            queue.push_front(pos + 1);
+            visited.insert(pos, "inc r16".to_string());
         } else if "00..1011".is_match(opcode) {
-            "dec r16".to_string()
+            //dec r16
+            queue.push_front(pos + 1);
+            visited.insert(pos, "dec r16".to_string());
         } else if "00..1001".is_match(opcode) {
-            "add hl, r16".to_string()
+            //add hl, r16
+            queue.push_front(pos + 1);
+            visited.insert(pos, "hl, r16".to_string());
         } else if "00...100".is_match(opcode) {
-            "inc r8".to_string()
+            //inc r8
+            queue.push_front(pos + 1);
+            visited.insert(pos, "inc r8".to_string());
         } else if "00...101".is_match(opcode) {
-            "dec r8".to_string()
+            //dec r8
+            queue.push_front(pos + 1);
+            visited.insert(pos, "dec r8".to_string());
         } else if "00...110".is_match(opcode) {
-            cartridge.get_next();
-            "ld r8, imm8".to_string()
+            //"ld r8, imm8"
+            let imm8 = cartridge.read_imm8(pos);
+            queue.push_front(pos + 2);
+            visited.insert(pos, format!("ld r8, {:x}", imm8));
         } else if "00000111".is_match(opcode) {
-            "rlca".to_string()
+            //rlca
+            queue.push_front(pos + 1);
+            visited.insert(pos, "rlca".to_string());
         } else if "00001111".is_match(opcode) {
-            "rrca".to_string()
+            //rrca
+            queue.push_front(pos + 1);
+            visited.insert(pos, "rrca".to_string());
         } else if "00010111".is_match(opcode) {
-            "rla".to_string()
+            //rla
+            queue.push_front(pos + 1);
+            visited.insert(pos, "rla".to_string());
         } else if "00011111".is_match(opcode) {
-            "rra".to_string()
+            //rra
+            queue.push_front(pos + 1);
+            visited.insert(pos, "rra".to_string());
         } else if "00100111".is_match(opcode) {
-            "daa".to_string()
+            //daa
+            queue.push_front(pos + 1);
+            visited.insert(pos, "daa".to_string());
         } else if "00101111".is_match(opcode) {
-            "cpl".to_string()
+            //cpl
+            queue.push_front(pos + 1);
+            visited.insert(pos, "cpl".to_string());
         } else if "00110111".is_match(opcode) {
-            "scf".to_string()
+            //scf
+            queue.push_front(pos + 1);
+            visited.insert(pos, "scf".to_string());
         } else if "00111111".is_match(opcode) {
-            "ccf".to_string()
+            //ccf
+            queue.push_front(pos + 1);
+            visited.insert(pos, "ccf".to_string());
         } else if "00011000".is_match(opcode) {
-            cartridge.get_next();
-            "jr imm8".to_string()
+            //jr imm8
+            panic!("unimplemented");
         } else if "001..000".is_match(opcode) {
-            cartridge.get_next();
-            "jr cond, imm8".to_string()
+            //cartridge.get_next();
+            //"jr cond, imm8".to_string()
+            panic!("unimplemented");
         } else if "00010000".is_match(opcode) {
-            "stop".to_string()
+            //"stop".to_string()
+            panic!("unimplemented");
         } else if "01110110".is_match(opcode) {
-            "halt".to_string()
+            //"halt".to_string()
+            panic!("unimplemented");
         } else if "01......".is_match(opcode) {
-            "ld r8, r8".to_string()
+            //ld r8, r8
+            queue.push_front(pos + 1);
+            visited.insert(pos, "ld r8, r8".to_string());
         } else if "10000...".is_match(opcode) {
-            "add a, r8".to_string()
+            //add a, r8
+            queue.push_front(pos + 1);
+            visited.insert(pos, "add a, r8".to_string());
         } else if "10001...".is_match(opcode) {
-            "adc a, r8".to_string()
+            //adc a, r8
+            queue.push_front(pos + 1);
+            visited.insert(pos, "adc a, r8".to_string());
         } else if "10010...".is_match(opcode) {
-            "sub a, r8".to_string()
+            //sub a, r8
+            queue.push_front(pos + 1);
+            visited.insert(pos, "sub a, r8".to_string());
         } else if "10011...".is_match(opcode) {
-            "sbc a, r8".to_string()
+            //sbc a, r8
+            queue.push_front(pos + 1);
+            visited.insert(pos, "sbc a, r8".to_string());
         } else if "10100...".is_match(opcode) {
-            "and a, r8".to_string()
+            //and a, r8
+            queue.push_front(pos + 1);
+            visited.insert(pos, "and a, r8".to_string());
         } else if "10101...".is_match(opcode) {
-            "xor a, r8".to_string()
+            //xor a, r8
+            queue.push_front(pos + 1);
+            visited.insert(pos, "xor a, r8".to_string());
         } else if "10110...".is_match(opcode) {
-            "or a, r8".to_string()
+            //or a, r8
+            queue.push_front(pos + 1);
+            visited.insert(pos, "or a, r8".to_string());
         } else if "10111...".is_match(opcode) {
-            "cp a, r8".to_string()
+            //cp a, r8
+            queue.push_front(pos + 1);
+            visited.insert(pos, "cp a, r8".to_string());
         } else if "11000110".is_match(opcode) {
-            cartridge.get_next();
-            "add a, imm8".to_string()
+            //cartridge.get_next();
+            //"add a, imm8".to_string()
+            panic!("unimplemented");
         } else if "11001110".is_match(opcode) {
-            cartridge.get_next();
-            "adc a, imm8".to_string()
+            //cartridge.get_next();
+            //"adc a, imm8".to_string()
+            panic!("unimplemented");
         } else if "11010110".is_match(opcode) {
-            cartridge.get_next();
-            "sub a, imm8".to_string()
+            //cartridge.get_next();
+            //"sub a, imm8".to_string()
+            panic!("unimplemented");
         } else if "11011110".is_match(opcode) {
-            cartridge.get_next();
-            "sbc a, imm8".to_string()
+            //cartridge.get_next();
+            //"sbc a, imm8".to_string()
+            panic!("unimplemented");
         } else if "11100110".is_match(opcode) {
-            cartridge.get_next();
-            "and a, imm8".to_string()
+            //cartridge.get_next();
+            //"and a, imm8".to_string()
+            panic!("unimplemented");
         } else if "11101110".is_match(opcode) {
-            cartridge.get_next();
-            "xor a, imm8".to_string()
+            //cartridge.get_next();
+            //"xor a, imm8".to_string()
+            panic!("unimplemented");
         } else if "11110110".is_match(opcode) {
-            cartridge.get_next();
-            "or a, imm8".to_string()
+            //cartridge.get_next();
+            //"or a, imm8".to_string()
+            panic!("unimplemented");
         } else if "11111110".is_match(opcode) {
-            cartridge.get_next();
-            "cp a, imm8".to_string()
+            //cp a, imm8
+            let imm8 = cartridge.read_imm8(pos);
+            queue.push_front(pos + 2);
+            visited.insert(pos, format!("cp a, [{:x}]", imm8));
         } else if "110..000".is_match(opcode) {
-            "ret cond".to_string()
+            //"ret cond".to_string()
+            panic!("unimplemented");
         } else if "11001001".is_match(opcode) {
-            "ret".to_string()
+            //"ret".to_string()
+            panic!("unimplemented");
         } else if "11011001".is_match(opcode) {
-            "reti".to_string()
+            //"reti".to_string()
+            panic!("unimplemented");
         } else if "110..010".is_match(opcode) {
             // jp cond, imm16
-            format!("jp cond, [{:x}]", cartridge.get_imm16())
+            let imm16 = cartridge.read_imm16(pos);
+            queue.push_front(pos + 3);
+            queue.push_front(imm16 as usize);
+            visited.insert(pos, format!("jp cond, [{:x}]", imm16));
         } else if "11000011".is_match(opcode) {
             // jp imm16
-            let jump_target = cartridge.get_imm16();
-            cartridge.set_position(jump_target as usize);
-            format!("jp [{:x}]", jump_target)
+            let jump_target = cartridge.read_imm16(pos);
+            queue.push_front(jump_target as usize);
+            visited.insert(pos, format!("jp [{}]", jump_target));
         } else if "11101001".is_match(opcode) {
-            "jp hl".to_string()
+            //"jp hl".to_string()
+            panic!("unimplemented");
         } else if "110..100".is_match(opcode) {
-            cartridge.get_next();
-            cartridge.get_next();
-            "call cond, imm16".to_string()
+            //cartridge.get_next();
+            //cartridge.get_next();
+            //"call cond, imm16".to_string()
+            panic!("unimplemented");
         } else if "11001101".is_match(opcode) {
-            cartridge.get_next();
-            cartridge.get_next();
-            "call imm16".to_string()
+            //cartridge.get_next();
+            //cartridge.get_next();
+            //"call imm16".to_string()
+            panic!("unimplemented");
         } else if "11...111".is_match(opcode) {
-            "rst tgt3".to_string()
+            //"rst tgt3".to_string()
+            panic!("unimplemented");
         } else if "11..0001".is_match(opcode) {
-            "pop r16stk".to_string()
+            //"pop r16stk".to_string()
+            panic!("unimplemented");
         } else if "11..0101".is_match(opcode) {
-            "push r16stk".to_string()
+            //"push r16stk".to_string()
+            panic!("unimplemented");
         } else if "11001011".is_match(opcode) {
-            "Prefix (see block below)".to_string()
+            //"Prefix (see block below)".to_string()
+            panic!("unimplemented");
         } else if "11100010".is_match(opcode) {
-            "ldh [c], a".to_string()
+            //"ldh [c], a".to_string()
+            panic!("unimplemented");
         } else if "11100000".is_match(opcode) {
-            cartridge.get_next();
-            "ldh [imm8], a".to_string()
+            //cartridge.get_next();
+            //"ldh [imm8], a".to_string()
+            panic!("unimplemented");
         } else if "11101010".is_match(opcode) {
-            cartridge.get_next();
-            cartridge.get_next();
-            "ld [imm16], a".to_string()
+            //ld [imm16], a
+            let imm16 = cartridge.read_imm16(pos);
+            queue.push_front(pos + 3);
+            visited.insert(pos, format!("ld [{:x}], a", imm16));
         } else if "11110010".is_match(opcode) {
-            "ldh a, [c]".to_string()
+            //"ldh a, [c]".to_string()
+            panic!("unimplemented");
         } else if "11110000".is_match(opcode) {
-            cartridge.get_next();
-            "ldh a, [imm8]".to_string()
+            //cartridge.get_next();
+            //"ldh a, [imm8]".to_string()
+            panic!("unimplemented");
         } else if "11111010".is_match(opcode) {
-            cartridge.get_next();
-            cartridge.get_next();
-            "ld a, [imm16]".to_string()
+            //ld a, [imm16]
+            let imm16 = cartridge.read_imm16(pos);
+            queue.push_front(pos + 3);
+            visited.insert(pos, format!("ld a, [{:x}]", imm16));
         } else if "11101000".is_match(opcode) {
-            cartridge.get_next();
-            "add sp, imm8".to_string()
+            //cartridge.get_next();
+            //"add sp, imm8".to_string()
+            panic!("unimplemented");
         } else if "11111000".is_match(opcode) {
-            cartridge.get_next();
-            "ld hl, sp + imm8".to_string()
+            //cartridge.get_next();
+            //"ld hl, sp + imm8".to_string()
+            panic!("unimplemented");
         } else if "11111001".is_match(opcode) {
-            "ld sp, hl".to_string()
+            //"ld sp, hl".to_string()
+            panic!("unimplemented");
         } else if "11110011".is_match(opcode) {
-            "di".to_string()
+            //"di".to_string()
+            panic!("unimplemented");
         } else if "11111011".is_match(opcode) {
-            "ei".to_string()
+            //"ei".to_string()
+            panic!("unimplemented");
         } else {
             panic!("Unknown opcode: {}", opcode)
         };
 
-        file.write(line.to_uppercase().as_bytes()).unwrap();
-        file.write("\n".as_bytes()).unwrap();
+    }
+
+    for entry in &visited {
+        println!("{}: {}", entry.0, entry.1.to_uppercase());
     }
 }
